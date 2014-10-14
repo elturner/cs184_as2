a9fe64c75b54fb9bfa8ffeebe1b0b2c4ea6cba41
Inguanzo <Inguanzo24@berkeley.edu>
2014-10-14 12:45:11 -0700

    Create parserTemp.cpp

---
+++ parserTemp.cpp
@@ -0,0 +1,78 @@
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <scene/camera.h>
+#include <scene/camera.cpp>
+#include <shape/shape.h>
+
+std::ifstream infile;
+infile.open("myFile.txt");
+
+for (std::string line; getline(infile, line);) {		
+	stringstream ss;
+	ss.str(line);
+	
+	string val;
+	ss >> val; /* "cam", "sph", etc. */
+
+	if(val.compare("cam") == 0)
+	{
+		/* this is a cam */
+		float ex ey ez llx lly llz lrx lry lrz ulx uly ulz urx ury urz;
+		ss >> ex >> ey >> ez >> llx >> lly >> llz >> lrx >> lry >> lrz >> ulx >> uly >> ulz >> urx >> ury >> urz; 
+	
+		scene.camera_t(): eye(ex, ey, ez),  //takes place of camera.h constructor
+			UL(ulx, uly, ulz), UR(urx, ury, urz), 
+			LL(llx, lly, llz), LR(lrx, lry, lrz)
+		{};
+	}
+	else if(val.compare("sph") == 0) {
+		float cx cy cz r;
+		ss >> cx >> cy >> cz >> r; 
+
+		shape.sphere_t(cx, cy, cz, r); //constructor from sphere.h
+	}
+	else if(val.compare("tri") == 0) {
+		float ax ay az bx by bz cx cy cz;
+		ss >> ax >> ay >> az >> bx >> by >> bz >> cx >> cy >> cz;
+
+		shape.trangle_t(ax, ay, az, bx, by, bz, cx, cy, cz); 
+	}
+	else if(val.compare("obj") == 0) {
+	/////
+	//// how to work with this? ////
+	/////
+	}
+	else if(val.compare("ltp") == 0) {
+		//////
+		float px py pz r g b [falloff]; //keep in brackets? where to specifiy lin/quad?
+		//////
+		ss >> px >> py >> pz >> r >> g >> b >> [falloff];
+
+		scene.light_t(true, px, py, pz, r, g, b);
+	}
+	else if(val.compare("ltd") == 0) {
+		float dx dy dz r g b;
+		ss >> dx >> dy >> dz >> r >> g >> b;
+
+		scene.light_t(true, dx, dy, dz, r, g, b);
+	}
+	else if(val.compare("lta") == 0) {
+		float r, g, b;
+		ss >> r >> g >> b;
+
+		scene.light_amb(r, g, b); //create ambient light
+	}
+	else if(val.compare("mat") == 0) {
+		
+	}
+	else if(val.compare("xft") == 0) {
+	}
+	else if(val.compare("xfr") == 0) {
+	}
+	else if(val.compare("xfs") == 0) {
+	}
+
+}
+
+infile.close();
